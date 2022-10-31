7a8a5a6bfd73c20df2d18bfd59dc36d0abca91a4
RexTrim <iwisdomikechi@gmail.com>
2022-10-31 13:28:05 +0100

    first commit

--- src/com/javacourse/EvenOdd.java
+++ src/com/javacourse/EvenOdd.java
@@ -1,18 +1,18 @@
-//package com.javacourse;
-//
-//import java.util.Scanner;
-//
-//public class EvenOdd {
-//        public static void main(String[] args){
-//            Scanner input = new Scanner(System.in);
-//            System.out.print("Enter an integer or whole number ? ");
-//            int num = input.nextInt();
-//
-//            double divisibleByTwo = num % 2;
-//
-//            if(divisibleByTwo == 0)
-//                System.out.println("The integer entered is even.");
-//            else
-//                System.out.println("The integer entered is odd.");
-//        }
-//}
+package com.javacourse;
+
+import java.util.Scanner;
+
+public class EvenOdd {
+        public static void main(String[] args){
+            Scanner input = new Scanner(System.in);
+            System.out.print("Enter an integer or whole number ? ");
+            int num = input.nextInt();
+
+            double divisibleByTwo = num % 2;
+
+            if(divisibleByTwo == 0)
+                System.out.println("The integer entered is even.");
+            else
+                System.out.println("The integer entered is odd.");
+        }
+}
